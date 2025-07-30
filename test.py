28c6af4826375a00b52c5256dc8e4a90a600a117
mohamedmakmandlac <mohamedb@mandlac.com>
2025-07-30 17:31:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -12,3 +12,8 @@ words = ["madam", "Race car", "hello", "nurses run"]
 for word in words:
     result = is_palindrome(word)
     print(f"{word} is a palindrome? {result}")
+if(0)
+{
+    else
+    {
+    }
