29b22614f1ed4a33e7741323a514ad9d744bfd34
mohamedmakmandlac <mohamedb@mandlac.com>
2025-07-31 17:11:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def is_palindrome(text):
 
 # Test cases
 words = ["madam", "Race car", "hello", "nurses run"]
+
 for word in words:
     result = is_palindrome(word)
     print(f"{word} is a palindrome? {result}")
