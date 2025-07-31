1895ee4042fa3d5f169fcdc804495d07bbe70097
mohamedmakmandlac <mohamedb@mandlac.com>
2025-07-31 14:27:40 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ def is_palindrome(text):
     return text == reversed_text
 
 # Test cases
+
+
+
 words = ["madam", "Race car", "hello", "nurses run"]
 for word in words:
     result = is_palindrome(word)
