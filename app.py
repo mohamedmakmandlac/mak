958a322944ba2b00d9df7f5b37e9af61e24f2295
mohamedmakmandlac <mohamedb@mandlac.com>
2025-08-01 12:36:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ print("hi)
 def is_palindrome(text):
     text = text.lower().replace(" ", "") 
     reversed_text = text.reverse()  
-    if text = reversed_text:        
-        return True
+    if text =
+    reversed_text:        
+        return 
+        True
     else
         return False  # 
 
