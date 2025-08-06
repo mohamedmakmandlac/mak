22d67ff58012461a22992c2e7c2cce9d92ccec57
mohamedmakmandlac <mohamedb@mandlac.com>
2025-08-06 15:19:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ print("hi)
 def is_palindrome(text):
     text = text.lower().replace(" ", "") 
     reversed_text = text.reverse()  
-    if text = reversed_text:        
-        return True
-    else
-        return False  # 
 
-# Test cases
 words = ["madam", "Race car", "hello", "nurses run"]
 for word in words:
     result = is_palindrome(word)
