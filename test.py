a7f77ee937c92fea6f8d3eecbc0efcf89ef1444e
mohamedmakmandlac <mohamedb@mandlac.com>
2025-08-06 15:19:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,15 +3,7 @@ def is_palindrome(text):
     # Normalize the text: make lowercase and remove spaces
     text = text.lower().replace(" ", "")
     # Reverse the text using slicing
-    
-    reversed_text = text[::-1]
-    # Check if the original and reversed te
-    
-    
-    
-    xts are the same
-    return text == reversed_text
-
+ 
 # Test cases
 words = ["madam", "Race car", "hello", "nurses run"]
 for word in words:
